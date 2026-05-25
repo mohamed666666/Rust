3be629ae602e3a345ef9842fb497c91cdce0c71c
Mohamed Ali <mohamedaswafi@gmail.com>
2026-05-26 00:44:14 +0300

    test against egp

--- pay/src/lib.rs
+++ pay/src/lib.rs
@@ -33,3 +33,9 @@ impl std::fmt::Display for Amount<USD>{
     }
 }
 
+
+impl std::fmt::Display for Amount<EGP>{
+    fn fmt(&self,f: &mut std::fmt::Formatter<'_>)->std::fmt::Result{
+        write!(f,"E{}",self.value)
+    }
+}
--- pay/src/main.rs
+++ pay/src/main.rs
@@ -1,10 +1,14 @@
-use pay::{Amount,USD};
+use pay::{Amount,USD,GPB,EGP};
 
 
 
 
 fn main() {
     let a:Amount<USD>=Amount::new(55);
+    println!("this is amount  {}" ,a);
+    let a:Amount<EGP>=Amount::new(55);
+    println!("this is amount  {}" ,a);
+    //let a:Amount<GPB>=Amount::new(55); error because display not impl for GPB
     println!("this is amount  {}" ,a);
 
 }
