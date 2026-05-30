92809f8a55feecebbeda3602d6432d14a0d220da
Mohamed Ali <mohamedaswafi@gmail.com>
2026-05-30 10:37:24 +0300

    _^ Rc<bool> cannot be shared between threads safely

--- async_r/src/main.rs
+++ async_r/src/main.rs
@@ -1,31 +1,31 @@
 use std::net::TcpListener;
 use std::time::Duration;
-
+use std::rc::Rc;
 
 fn main() {
-    let mut t2shutdown=false;
+    let mut t2shutdown:Rc<bool>=Rc::new(false);
 
-    let t1= std::thread::spwan(|| {//spwan dunction return refrance called johnhandle object used as remote controll over the thread
+    let t1= std::thread::spawn(|| {//spawn dunction return refrance called johnhandle object used as remote controll over the thread
          let listener=TcpListener::bind("0.0.0.0:4000").unwrap();
     println!("Server Start listen on : {:?}",listener);
     let (stream,socket)=listener.accept().unwrap();
     println!("connection recieved on : {:?}",socket);
         stream
     }
-    )
+    );
 
-     std::thread::spwan(|| {//spwan dunction return refrance called johnhandle object used as remote controll over the thread
-         while !t2shutdown{
+     std::thread::spawn(|| {//spawn dunction return refrance called johnhandle object used as remote controll over the thread
+         while !*t2shutdown{//
             println!("Hello from the Secound thread ");
             std::thread::sleep(Duration::from_secs(2));
          }
 
     }
-    )
+    );
     
 
     let stream=t1.join();//wait untill the thread finish and take the returned data 
-    t2shutdown=true;
+    *t2shutdown=true;
     println!("closed all threads ");
 
 }
